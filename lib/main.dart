19cdef71294cbf09014d508c804d52de0d5efa4a
SeongEun <bongfam2@naver.com>
2023-08-29 17:29:02 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            color: Colors.orange,
+          ),
+          child: const Center(
+            child: Text('Splash Screen'),
+          ),
+        ),
+      ),
+    );
+  }
+}
